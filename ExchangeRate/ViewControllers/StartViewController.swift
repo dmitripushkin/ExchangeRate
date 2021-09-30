e6f2307d5c76351802177e9b5f6013318260aa84
Dmitri Gordeev <dimapushkin@gmail.com>
2021-09-30 03:05:52 +0300

    tuning cell view

--- ExchangeRate/ViewControllers/StartViewController.swift
+++ ExchangeRate/ViewControllers/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  ExchangeRate
 //
 //  Created by Повелитель on 29.09.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ExchangeRate/Views/RateViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RateViewCell.swift
+//  ExchangeRate
+//
+//  Created by Повелитель on 30.09.2021.
+//
+
+import UIKit
+
+class RateViewCell: UITableViewCell {
+    @IBOutlet var nominal: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var rate: UILabel!
+}
